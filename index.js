b25fe37dbc88b1cfe2754c5b7ce0436470951b6c
Gabriel Vergara <gvergara182@gmail.com>
2025-08-04 12:52:23 -0400

    fin sección 23

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { dbConnection } = require('./database/config');
+const dotenv = require('dotenv').config();
+const cors = require('cors');
+
+// console.log(process.env)
+
+// Crear el servidor de express
+const app = express();
+
+// Base de datos
+dbConnection();
+
+// CORS
+app.use( cors() );
+
+// Directorio Público
+app.use(express.static('public'));
+
+// Lectura y parseo del body
+app.use( express.json())
+
+// Rutas
+app.use('/api/auth', require('./routes/auth'))
+// TODO: auth // crear,login, renew
+// TODO: CRUD: Eventos
+
+// Escuchar peticiones
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
+})
